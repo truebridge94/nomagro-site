4e6f54152bb9dbb72d1c39f69ea31215f4cc6e37
Aboobakar Sabiu <sabiuabu@gmail.com>
2025-08-18 14:03:50 +0100

    Updated server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -103,8 +103,9 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   logger.info(`Server running on port ${PORT} in ${process.env.NODE_ENV} mode`);
 });
+
 
 export default app;
